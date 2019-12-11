e796a675ba59b6c28dd3eb1fb0126eadb194f3d3
Ankit Srivastava <42239246+ankitsri98@users.noreply.github.com>
2019-12-11 23:19:12 +0530

    Update (GCD)-EUCLID ALGO.cpp

--- (GCD)-EUCLID ALGO.cpp
+++ (GCD)-EUCLID ALGO.cpp
@@ -3,8 +3,14 @@ using namespace std;
 
 int gcd(int a,int b)
 {
+    //special case
+    if(a<b)
+    {
+        return gcd(b,a);
+    }
     if(b==0)
         return a;
+    
     return gcd(b,a%b);
     /*
     or YOU CAN ALSO USE IN BUILD FUNCTION FOR GCD WHICH FOLLOW EUCLID ALGORITHM
