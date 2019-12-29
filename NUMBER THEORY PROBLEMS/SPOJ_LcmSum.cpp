9a5d076520568b77282a54cf544f45a41bedeec6
Ankit Srivastava <42239246+ankitsri98@users.noreply.github.com>
2019-12-30 02:08:52 +0530

    Euler Totient Function Based Problem

---
+++ NUMBER THEORY PROBLEMS/SPOJ_LcmSum.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define ll long long int
+
+ll sumphi[1000002]={0};
+void eulerSeive(ll n)
+{
+    ll a[n+2];
+    for(ll i=0;i<=n+1;i++)
+    {
+        a[i]=i;
+    }
+    for(ll i=2;i<=n;i++)
+    {
+        if(a[i]==i)
+        {
+            a[i]=i-1;
+            for(ll j=2*i;j<=n;j+=i)
+            {
+                a[j]=(a[j]*(i-1))/i;
+            }
+        }
+        else
+        {
+            continue;
+        }
+    }
+    /*for(ll i=1;i<=n;i++)
+    cout<<"EULER TOTIENT PHI VALUE FOR "<<i<<" IS "<<a[i]<<endl;
+    cout<<endl;*/
+
+    for(ll i=1;i<=n;i++)
+    {
+        for(ll j=i;j<=n;j+=i)
+        {
+            sumphi[j]+=(i*a[i]); //coz i sabka divisor hain na  toh saari pos par i as a divisor add hoga
+        }
+    }
+}
+
+void func(long long n)
+{
+    eulerSeive(1000000);
+	ll x=((sumphi[n]+1)*n)/2;
+    cout<<x<<endl;
+}
